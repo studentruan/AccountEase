ddf7dd45c7e73842b54026a9cf676af4e764bc79
阮予 <ruanyu@ruanyudeMacBook-Air.local>
2025-04-17 19:15:10 +0800

    nothing

--- src/main/java/com/myapp/Main.java
+++ src/main/java/com/myapp/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,11 +16,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         instance = this;
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ledgermanager.fxml"));
-        VBox root = loader.load();
-//        mainPane = loader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+        mainPane = loader.load();
 
-        Scene scene = new Scene(root, 1490, 900);
+        Scene scene = new Scene(mainPane, 1490, 900);
         scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("AccountEase");
